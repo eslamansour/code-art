d4f61e3a26578bc38f93088f5f62d6a4cf8e7fb4
eslamansour <eslam.mansour15@gmail.com>
2019-11-03 11:31:40 +0200

    login, signup

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $(".dropDown-navLink").click(function() {
     $(".nav-link-dropdown").slideToggle("fast");
   });
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
 });
