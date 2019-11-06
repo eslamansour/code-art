17b2c2c465d92b02d4b4f3a2af82e8a97ff0558b
eslamansour <eslam.mansour15@gmail.com>
2019-11-07 01:14:52 +0200

    14 page, serv-priv-uers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+  $(".dropDown-navLink_login").click(function() {
+    $(".user_card-dropdown").slideToggle("fast");
+  });
   $(".dropDown-navLink").click(function() {
     $(".nav-link-dropdown").slideToggle("fast");
   });
