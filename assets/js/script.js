27a8fa0a9e94526ce79878e2d11eacf2932ecc48
EsLaMaNsOuR  <eslam.mansour15@gmail.com>
2019-12-06 15:29:08 +0200

    footer is done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
 
+  var state = 1;
   $(".dropDown-navLink_login").click(function() {
     $(".user_card-dropdown").slideToggle("fast");
+    
+    
   });
   $(".dropDown-navLink").click(function() {
     $(".nav-link-dropdown").slideToggle("fast");
+    console.log("tog");
+    state = 1;
   });
   $(function () {
     $('[data-toggle="popover"]').popover()
